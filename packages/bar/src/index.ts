859253648b469374369117520c5ec0fe45561f81
chenhaoli <chenhaoli@bytedance.com>
2023-01-20 14:51:56 +0800

    refactor: convention

--- packages/bar/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { meaningOfLife } from "@nomadland/foo";
-
-// eslint-disable-next-line no-console
-console.log(meaningOfLife);
--- packages/foo/src/index.ts
+++ packages/foo/src/index.ts
@@ -1,1 +1,4 @@
-export const meaningOfLife = 42;
+import { meaningOfLife } from "@nomadland/foo";
+
+// eslint-disable-next-line no-console
+console.log(meaningOfLife);
