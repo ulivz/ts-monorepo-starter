842ab457367a3258c88e1f2848f230a0c617a32f
chenhaoli <chenhaoli@bytedance.com>
2025-04-27 02:42:20 +0800

    feat: remove eslint and lock all dependencies' version

--- packages/foo/__tests__/index.test.ts
+++ packages/foo/__tests__/index.test.ts
@@ -5,13 +5,13 @@ describe('foo', () => {
   it('should use shared module', () => {
     // Mock console.log
     const consoleSpy = vi.spyOn(console, 'log');
-    
+
     // Import module to trigger the console.log
     require('@default-scope/foo');
-    
+
     // Verify console.log was called with NAME
     expect(consoleSpy).toHaveBeenCalledWith(NAME);
-    
+
     // Clean up
     consoleSpy.mockRestore();
   });
